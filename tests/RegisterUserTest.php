b388d38d2e5d526a45c37f21da9212eb44d9c6ea
Andreas Koumato <andreaskoumato@gmail.com>
2025-08-07 16:34:01 -0600

    new updates

--- tests/RegisterUserTest.php
+++ tests/RegisterUserTest.php
@@ -8,10 +8,37 @@ class RegisterUserTest extends WebTestCase
 {
     public function testSomething(): void
     {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/');
 
-        $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Hello World');
+        /*
+        * 1. Créer un faux client (navigateur) de pointer vers une URL
+        * 2. Remplier les champs de mon formulaire d'inscription
+        * 3. Est-ce que tu peux vérifier si dans ma page j'ai le message (alerte) suivante : Votre compte est correctement créé
+        */
+
+        //1
+
+        $client = static::createClient();
+        $client->request("GET","/inscription");
+
+        // 2 (fistname, lastname, email, password, confirmation)
+
+        $client->submitForm("Valider", [
+
+            "register_user[email]" => "andreas@mossosouk.com",
+            "register_user[plainPassword][first]" => "Azerty32$",
+            "register_user[plainPassword][second]" => "Azerty32$",
+            "register_user[firstname]"  => "Andreas",
+            "register_user[lastname]" => "KOUMATO",
+        ]);
+
+        // follow
+        $this->assertResponseRedirects("/connexion");
+        $client->followRedirect();
+
+        // 3 
+
+        $this->assertSelectorExists('div:contains("Votre compte est correctement créé, veuillez vous connecter!")') ;
+        
+
     }
 }
