14a17ce37861ffb00de78c91425fd58edc91be46
Andreas Koumato <andreaskoumato@gmail.com>
2025-08-13 11:51:41 -0600

    Last updates

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Address;
+use App\Repository\AddressRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -9,9 +11,15 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Form\PasswordUserType;
+use App\Form\AddressUserType;
 
 final class AccountController extends AbstractController
 {
+
+    public function __construct(private EntityManagerInterface $em) {
+        $this->em = $em;
+    }
+
     #[Route('/compte', name: 'app_account')]
     public function index(): Response
     {
@@ -19,7 +27,7 @@ final class AccountController extends AbstractController
     }
 
     #[Route('/compte/modifier-mot-de-passe', name: 'app_account_modify_pwd')]
-    public function password(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $em): Response
+    public function password(Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
         $user = $this->getUser();
         $form = $this->createForm(PasswordUserType::class, $user, [
@@ -30,7 +38,7 @@ final class AccountController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             
-            $em->flush();
+            $this->em->flush();
 
             $this->addFlash(
                 'success',
@@ -40,6 +48,63 @@ final class AccountController extends AbstractController
 
         return $this->render('account/password.html.twig', [
             'modifyPwd'=> $form->createView(),
+        ]);
+    }
+
+    #[Route('/compte/adresses', name: 'app_account_addresses')]
+    public function addresses(): Response
+    {
+        return $this->render('account/addresses.html.twig');
+    }
+
+    #[Route('/compte/adresses/delete/{id}', name: 'app_account_addresses_delete')]
+    public function addressDelete($id, AddressRepository $repo): Response
+    {
+        $address = $repo->findOneById($id);
+
+        if (!$address OR $address->getUser() !== $this->getUser()) {
+            return $this->redirectToRoute('app_account_addresses');
+        }
+
+        $this->em->remove($address);
+        $this->em->flush();
+        $this->addFlash('success', 'Votre adresse est correctement supprimée.');
+
+        return $this->redirectToRoute('app_account_addresses');
+    }
+
+    #[Route('/compte/adresse/ajouter/{id}', name: 'app_account_address_form', defaults: ['id'=> null])]
+    public function addressForm(Request $request, $id, AddressRepository $repo): Response
+    {
+        
+        if ($id) {
+
+            $address = $repo->findOneById($id);
+            if( !$address OR $address->getUser() != $this->getUser()) { 
+                return $this->redirectToRoute('app_account_addresses');
+            }
+
+        } else {
+            $address = new Address();
+            $address->setUser($this->getUser());
+        }
+
+        
+        $form = $this->createForm(AddressUserType::class, $address);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $this->em->persist($address);
+            $this->em->flush();
+
+            $this->addFlash('success', 'Votre adresse est correctement sauvegardée.');
+
+            return $this->redirectToRoute('app_account_addresses');
+        }
+
+        return $this->render('account/addressForm.html.twig', [
+            'addressForm' => $form
         ]);
     }
 }
